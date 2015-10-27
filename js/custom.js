6368c03eec7146a8b216f421ca8ba03f49d3baa6
Anna <annakrygier@hotmail.com>
2015-10-26 17:31:21 -0700

    beautify css and js

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
-
-$(".sidebar ul:first-child li").click(function(){
-    var color = $(this).css("background-color");
-    $(".logo img").css("border-color", color);
-    $(".name p").css("color", color);
-    $("h2").css("color", color);
-});
+    $(".sidebar ul:first-child li").click(function(){
+        var color = $(this).css("background-color");
+        $(".logo img").css("border-color", color);
+        $(".name p").css("color", color);
+        $("h2").css("color", color);
+    });
 });
