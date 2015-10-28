1c3f0c34582585d301c6547914d2805ef1d71c85
Anna <annakrygier@hotmail.com>
2015-10-27 22:14:16 -0700

    beautified js

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
-    $(".sidebar ul:first-child li").click(function(){
+$(document).ready(function() {
+    $(".sidebar ul:first-child li").click(function() {
         var color = $(this).css("background-color");
-        $(".logo img").css("border-color", color);
+        $("h2").css("background-color", color);
         $(".name p").css("color", color);
-        $("h2").css("color", color);
+        $(".headline").css("border-color", color);
     });
 });
